715031c9f66380ac002f18cdfdfb67c7bd46fd8a
guiontrauma-maker <guiontrauma@gmail.com>
2026-08-27 20:38:02 +0000

    commit mensaje

--- src/utils/whatsapp.js
+++ src/utils/whatsapp.js
@@ -1,15 +1,6 @@
 const WHATSAPP_NUMBER = "5559032021";
 
-const WHATSAPP_MESSAGE = `Hola, quiero solicitar una evaluación gratuita de mi caso.
-
-Para poder conocer mejor mi situación y recibir orientación, comparto los siguientes datos:
-
-Nombre completo:
-Teléfono:
-Correo electrónico:
-Tipo de fraude:
-Monto aproximado de la pérdida:
-Cuéntanos brevemente qué ocurrió:`;
+const WHATSAPP_MESSAGE = `Hola, quiero un analisis gratuito de mi caso`;
 
 export const abrirWhatsApp = () => {
     const mensaje = encodeURIComponent(WHATSAPP_MESSAGE);
